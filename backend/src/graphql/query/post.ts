ef219edbac7964d181f089a04f1c505966eb1ff1
Dechapon Nunsen <Dechapon.n@hotmail.com>
2022-05-28 00:44:40 +0700

    add newest posts api

--- backend/src/graphql/query/post.ts
+++ backend/src/graphql/query/post.ts
@@ -1,6 +1,15 @@
-import { PostTC } from "../../model/Post"
-
+import { Post, PostTC } from "../../model/Post"
+import { schemaComposer } from "graphql-compose"
 export const postById = PostTC.mongooseResolvers.findById()
 export const post = PostTC.mongooseResolvers.findOne()
 export const posts = PostTC.mongooseResolvers.findMany()
 export const postCount = PostTC.mongooseResolvers.count()
+
+export const newestPosts = schemaComposer.createResolver({
+    name:'newestPosts',
+    kind:'query',
+    type:PostTC.mongooseResolvers.findMany().getType(),
+    resolve: async () => {
+        return await Post.find().lean().sort( { createdAt : -1 }).limit(5)
+    }
+})

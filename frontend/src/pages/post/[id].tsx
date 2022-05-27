eaf843159f86cdb4401f55f0df753ef9749a1e26
gwattana <genwtn123@gmail.com>
2022-05-28 02:35:10 +0700

    fix delete

--- frontend/src/pages/post/[id].tsx
+++ frontend/src/pages/post/[id].tsx
@@ -12,108 +12,6 @@ import { Rating } from "../../types/Rating"
 import { useSession, getSession } from "next-auth/react"
 import { ME } from "../../graphql/me"
 
-const postData = [
-    {
-        title: "Newest Post 1",
-        author: "Name Lastname",
-        contact: "contact",
-        permission: "private",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
-        createDate: "09/02/2565",
-        imgUrl: "http://www.168virtualschool.com/images/No_image_available.png",
-        tag: [
-            "Tag1",
-            "Tag2",
-            "Tag3",
-            "Tag4",
-            "Tag5",
-        ],
-        rating: 4,
-        avgRating: 4,
-        comment: [
-            {
-                author: "Name Lastname",
-                createdDate: new Date(),
-                comment: "Good",
-            }
-        ]
-    },
-    {
-        title: "Newest Post 2",
-        author: "Name Lastname",
-        contact: "contact",
-        permission: "private",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
-        createDate: "09/02/2565",
-        imgUrl: "http://www.168virtualschool.com/images/No_image_available.png",
-        tag: [
-            "Tag1",
-            "Tag2",
-            "Tag3",
-            "Tag4",
-            "Tag5",
-        ],
-        rating: 3,
-        avgRating: 4,
-        comment: [
-            {
-                author: "Name Lastname",
-                createdDate: new Date(),
-                comment: "Good",
-            }
-        ]
-    },
-    {
-        title: "Newest Post 3",
-        author: "Name Lastname",
-        contact: "contact",
-        permission: "private",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
-        createDate: "09/02/2565",
-        imgUrl: "http://www.168virtualschool.com/images/No_image_available.png",
-        tag: [
-            "Tag1",
-            "Tag2",
-            "Tag3",
-            "Tag4",
-            "Tag5",
-        ],
-        rating: 5,
-        avgRating: 4,
-        comment: [
-            {
-                author: "Name Lastname",
-                createdDate: new Date(),
-                comment: "Good",
-            }
-        ]
-    },
-    {
-        title: "Newest Post 3",
-        author: "Name Lastname",
-        contact: "contact",
-        permission: "private",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
-        createDate: "09/02/2565",
-        imgUrl: "http://www.168virtualschool.com/images/No_image_available.png",
-        tag: [
-            "Tag1",
-            "Tag2",
-            "Tag3",
-            "Tag4",
-            "Tag5",
-        ],
-        rating: 5,
-        avgRating: 4,
-        comment: [
-            {
-                author: "Name Lastname",
-                createdDate: new Date(),
-                comment: "Good",
-            }
-        ]
-    },
-]
 
 interface Props {
     id: string,
@@ -202,16 +100,11 @@ const PostDetailPage: NextPage<Props> = ({ id }) => {
     const { loading:meLoading, error:meError, data:meData, refetch:meRefetch } = useQuery(ME)
 
     const getMyId = useCallback(async () => {
-        // const me = await axios.get(`${process.env.NEXT_PUBLIC_API_LINK}/me`, {
-        //     headers: {
-        //         Authorization: `Bearer ${session?.accessToken}`
-        //     }
-        // })
         if(meData){
-            if (meData._id === data?.postById.authorId) {
+            if (meData.me._id === data?.postById.authorId) {
                 setOwner(true)
             }
-            setMyId(meData._id)
+            setMyId(meData.me._id)
         }
     }, [meData, data])
 
@@ -267,7 +160,6 @@ const PostDetailPage: NextPage<Props> = ({ id }) => {
             <Stack className={styles.container}>
                 <PostDetail
                     post={data?.postById}
-                    postData={postData[1]}
                     addComment={(newComment: string) => handleComment(newComment)}
                     ratePost={(rating: number) => handleCreateRating(rating)}
                     deleteComment={(comment: string) => deleteComment(comment)}
